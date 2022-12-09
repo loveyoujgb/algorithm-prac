ea34909d67604e44da16bb97bd15fe67eb197dc3
DESKTOP-DKJENIO\skfl2 <kimmiri235@daum.net>
2022-12-09 23:24:48 +0900

    Algorithm-budget

---
+++ etcTest/budget.js
@@ -0,0 +1,14 @@
+function solution(d, budget) {
+  var answer = 0;
+  let array = d.sort();
+  for (let i = 0; i < d.length; i++) {
+    if (budget - array[i] >= 0) {
+      budget = budget - array[i];
+      answer += 1;
+    }
+  }
+  return answer;
+}
+solution([1, 3, 2, 5, 4], 9);
+solution([2, 2, 3, 3], 10);
+solution([12, 5, 12, 5], 12);
